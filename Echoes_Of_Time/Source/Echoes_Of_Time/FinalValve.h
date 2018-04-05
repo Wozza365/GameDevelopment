4673de5fad3bf6ab0f64f167174cd7e531f27788
Venree <Michele.felice90@gmail.com>
2018-04-05 05:02:25 +0100

    Final room valve code, unfinished
    Just pushing it up to back up to git, you never know...

---
+++ Echoes_Of_Time/Source/Echoes_Of_Time/FinalValve.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FinalValve.generated.h"
+
+UCLASS()
+class ECHOES_OF_TIME_API AFinalValve : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFinalValve();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Valve Comp")
+		UStaticMeshComponent* valve;
+
+	UPROPERTY(VisibleAnywhere, Category = "Box Comp")
+		class UBoxComponent* boxComp;
+
+	UFUNCTION()
+		void OpenValve(float dt);
+
+	UFUNCTION()
+		void ToggleValve();
+
+	UFUNCTION()
+		void CloseValve(float dt);
+
+	bool opening;
+	bool closing;
+	float rotSpeed;
+	float maxRot;
+	float addRot;
+	float currentRot;
+};
